692347dba1201f20e73cacbdb65315ec30b52e8d
Evan <ryderevan@proton.me>
2026-02-07 18:05:56 -0600

    Updating to reflect new name

---
+++ Slash.py
@@ -0,0 +1,35 @@
+#Import classes, vehicle class is for all vehicle driving functions, time is for loop timing
+from time import sleep
+import Vehicle
+
+#Set input pins
+throttle_pin = 23
+steering_pin = 24
+servo_pin = 18
+
+#initiatlize vehicle object
+BajaRey = Vehicle.vehicle(throttle_pin, steering_pin, servo_pin)
+
+# Change this to switch modes, Mode 1 for pass through, Mode 2 to collect data and mode 3 to run linear model inference
+DRIVE_MODE = 1    
+BajaRey.set_drive_mode(DRIVE_MODE)
+
+# Initialize control loop parameters
+loop_rate = 50.0  # Hz
+dt = 1.0 / loop_rate  # seconds per iteration
+i = 0
+
+#Main drive loop
+try:
+    while True:
+        # Execute one control step  
+        BajaRey.step(i, dt)
+        
+        # Increment iteration counter
+        i += 1
+        sleep(dt)
+
+
+#Break loop if Ctrl+C is pressed
+except KeyboardInterrupt:
+    BajaRey.shutdown()
